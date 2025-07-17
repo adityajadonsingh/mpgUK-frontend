e086bd0c41ace3dcf08c0224f5fe9667f4f24895
Aditya Jadon <51057603+adityajadonsingh@users.noreply.github.com>
2025-07-17 12:16:43 +0530

    Testimonials section complete

--- src/components/home/TestimonialSection.tsx
+++ src/components/home/TestimonialSection.tsx
@@ -10,18 +10,21 @@ interface TestimonialProps {
   data: Testimonial[];
 }
 
-export default function TestimonialSection({ data }: TestimonialProps ) {
+export default function TestimonialSection({ data }: TestimonialProps) {
   console.log(data);
   return (
     <>
-      <section className="testimonials">
+      <section className="testimonials mt-20 mb-10">
         <div className="container">
+          <h3 className="heading text-center text-3xl font-semibold ">
+            What Our Clients Say About Us
+          </h3>
           <div className="slider">
             <Swiper
               pagination={{ clickable: true }}
               modules={[Autoplay, Navigation]}
               navigation
-              slidesPerView={4}
+              slidesPerView={3}
               spaceBetween={20}
               className="h-full w-full"
               autoplay={{
@@ -29,28 +32,48 @@ export default function TestimonialSection({ data }: TestimonialProps ) {
                 disableOnInteraction: false,
               }}
               speed={800}
+              loop={true}
               effect="slide"
             >
               {data.map((testimonial) => {
-                console.log(testimonial)
+                console.log(testimonial);
                 return (
                   <SwiperSlide
                     key={testimonial.id}
                     className="relative w-full h-full"
                   >
-                    <div className="card">
-                      <div className="flex">
-                        <div className="img-box">
+                    <div className="card rounded-md min-h-[280px] shadow-md">
+                      <div className="flex gap-x-3">
+                        <div className="img-box w-2/12 relative">
                           <Image
                             src={testimonial.profile_image}
                             alt={testimonial.name}
-                            className="w-full h-full object-cover object-center"
-                            fill
+                            className=" rounded-full object-cover object-center"
+                            width={40}
+                            height={40}
                           />
                         </div>
-                        <span className="text-lg font-medium">
-                          {testimonial.name}
-                        </span>
+                        <div className="w-10/12">
+                          <span className="text-lg block font-medium">
+                            {testimonial.name}
+                          </span>
+                          <div className="flex items-center text-yellow-500 mb-1">
+                            {[...Array(5)].map((_, i) => (
+                              <i
+                                key={i}
+                                className={
+                                  i < testimonial.rating
+                                    ? "bi bi-star-fill"
+                                    : "bi bi-star"
+                                }
+                              ></i>
+                            ))}
+                          </div>
+                          <hr className="my-2 text-[#ccc]" />
+                          <span className="block">
+                            {testimonial.testimonial}
+                          </span>
+                        </div>
                       </div>
                     </div>
                   </SwiperSlide>
